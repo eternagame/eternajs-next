66c1f717c3ce8c54e14e4344fa5880f14d86a838
Amine Rehioui <amine@franticsoftware.com>
2022-03-21 14:16:06 +0100

    Prototype wip

--- packages/designer/src/index.ts
+++ packages/designer/src/index.ts
@@ -1,7 +1,24 @@
+import * as PIXI from 'pixi.js';
 import { computeRNA } from '@eternagame/rna';
 import drawRNA from '@eternagame/rna-viz-2d';
 
+import './style.css';
+
 export default function designer() {
+  // Setup PIXI
+  const app = new PIXI.Application({
+    resolution: devicePixelRatio,
+  });
+
+  document.body.appendChild(app.view);
+
+  function resize() {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+  }
+
+  resize();
+  window.addEventListener('resize', resize);
+
   const rna = computeRNA(
     [-1, 13, 12, 11, 10, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1],
     [1, 1, 1, 3, 4, 1, 1, 1, 1, 4, 3, 2, 3, 4, 1]
